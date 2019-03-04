644f5f38fb89ed2258caf1230f621de1bebb80ee
Grishy <mailgrishy@gmail.com>
2019-03-04 16:23:38 +0300

    lab2

--- Func/lab2/ser/grishy.lisp
+++ Func/lab2/ser/grishy.lisp
@@ -13,18 +13,16 @@
 ;; (print (addToTail 4 foo))
 
 ;; (defun splitBy (lst n k &optional r1 (nn 0) r2 (kk 0)) (
-;;     (when 
-;;         (null lst) 
-;;         (return (list r1  r2)))
+;;     (when ((null lst) (list r1 r2))
 ;;     (setq nextElem (car lst))   
 ;;     (setq otherElem (cdr lst))   
-;;     (return (cond
+;;     (return (cond 
 ;;         ((< nn n) (splitBy otherElem n k (addToTail nextElem r1) (+ nn 1) r2 kk))
 ;;         ((< kk k) (splitBy otherElem n k r1 nn (addToTail nextElem r2) (+ kk 1)))                                                                                     
 ;;         (t (splitBy otherElem n k (addToTail nextElem r1) 1 r2 0))))))
 
 (defun splitBy (lst n k &optional r1 (nn 0) r2 (kk 0))
-  (cond ((null lst) (list r1 r2))   
+  (cond ((null lst) (list r1 r2))
         ((< nn n) (splitBy (cdr lst) n k (addToTail (car lst) r1) (+ nn 1) r2 kk))
         ((< kk k) (splitBy (cdr lst) n k r1 nn (addToTail (car lst) r2) (+ kk 1))) 
         (t (splitBy (cdr lst) n k (addToTail (car lst) r1) 1 r2 0)))) 
