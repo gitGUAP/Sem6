15d90f55afc55178e6843b4fd23691103b3b6adf
Grishy <mailgrishy@gmail.com>
2019-03-04 16:27:33 +0300

    Update dan.lisp

--- Func/lab2/dan/dan.lisp
+++ Func/lab2/dan/dan.lisp
@@ -1,25 +1,20 @@
-(print
-    (list
-        (CADR '(A X C))
-        (CADR '(F Y))
-        (CAR '(Z))
-    )
-)
-
-(print
-    (list
-        (CADADR '((A) (B X) (C D)))
-        (CAAR '((Y (Z))))
-        (CAADAR '((Y (Z))))
-    )
-)
+;; 14. Разработать функцию, преобразующую исходный список, в список «луковицу».
+;; Например:
+;; Вход: (3 2 1 2 3).
+;; Выход: (3 (2 (1) 2) 3). 
 
 
-(print
-    (list
-        (CADR '(A X B Y))
-        (CADDDR '(A X B Y))
-        (CADR '(F Z))
-    )
-)
+(defun onion (L)
+ (cond
+  ((null L) nil)
+  ((null (cdr L)) L)
+  (T
+   (cons
+    (car L)
+    (cons
+     (onion (cdr (butlast L)))
+     (last L))))))
 
+(trace onion)
+(print (onion '(3 2 1 2 3)))
+(print '(3 2 1 2 3))
