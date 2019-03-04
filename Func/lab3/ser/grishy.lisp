225b8ee6b7bf29266e069c3b29d72943b7d65b73
Grishy <mailgrishy@gmail.com>
2019-03-04 16:43:40 +0300

    Update grishy.lisp

--- Func/lab3/ser/grishy.lisp
+++ Func/lab3/ser/grishy.lisp
@@ -6,12 +6,96 @@
 ;; этой величины для всех вершин графа. Если таких вершин несколько, вывести
 ;; их все
 
-(defun splitBy (lst n k &optional r1 (nn 0) r2 (kk 0))
-  (cond ((null lst) (list r1 r2))
-        ((< nn n) (splitBy (cdr lst) n k (addToTail (car lst) r1) (+ nn 1) r2 kk))
-        ((< kk k) (splitBy (cdr lst) n k r1 nn (addToTail (car lst) r2) (+ kk 1))) 
-        (t (splitBy (cdr lst) n k (addToTail (car lst) r1) 1 r2 0)))) 
+(defun F (Net)
+ ((lambda (enum counter)
+   ((lambda (averages)
+     ((lambda (mid)
+       ((lambda (deviations)
+         (EXTRACT_MIN (apply 'min deviations) deviations enum))
+        (mapcar
+        #'(lambda (dist)
+          (abs (- dist mid)))
+         averages)))
+      (/ (apply '+ averages) counter)))
+    (mapcar
+    #'(lambda (node)
+      (/
+       (apply '+
+        (mapcar
+        #'(lambda (other)
+          (DIJKSTRA Net node other))
+         (remove node enum :test 'EQUAL)))
+       (1- counter)))
+     enum)))
+  (mapcar 'car Net)
+  (length Net)))
 
-(trace splitBy)
-(print (splitBy '(1 2 3 4 5 6 7 8 9 10 11) 2 3))
-(print '((1 2 6 7 11) (3 4 5 8 9 10)))
+(defun EXTRACT_MIN (minimal deviations enum)
+ (if deviations
+  ((lambda (test result)
+    (if test (cons (car enum) result) result))
+   (equal (car deviations) minimal)
+   (EXTRACT_MIN minimal (cdr deviations) (cdr enum)))))
+
+(defun DIJKSTRA (Net Init Term
+ &optional (Tmp nil) (Fix (list (cons Init 0))))
+ ((lambda (fix_label fix_value)
+   (if (equal fix_label Term) fix_value
+    (apply 
+     #'(lambda (newTmp newFix)
+      (DIJKSTRA Net Init Term newTmp newFix))
+     (TRANSFER_MIN
+      (UPDATE_Tmp Tmp Fix (cdr (assoc fix_label Net)))
+      Fix))))
+  (caar Fix)
+  (cdar Fix)))
+
+(defun UPDATE_Tmp (Tmp Fix Links)
+ (if Links
+  ((lambda (link_label link_value)
+    (UPDATE_Tmp
+     (if (assoc link_label Fix :test 'EQUAL) Tmp
+      ((lambda (link mark)
+        (if link
+         (subst
+          (cons link_label (min mark (cdr link)))
+          link
+          Tmp
+          :test 'EQUAL)
+         (cons (cons link_label mark) Tmp)))
+       (assoc link_label Tmp :test 'EQUAL)
+       (+ link_value (cdar Fix))))
+     Fix
+     (cdr Links)))
+   (caar Links)
+   (cdar Links))
+  Tmp))
+
+(defun TRANSFER_MIN (Tmp Fix)
+ (if Tmp
+  (if (cdr Tmp)
+   (apply
+    #'(lambda (elem newTmp newFix)
+     (if (< (cdr elem) (cdar newFix))
+      (list
+       (cons (car newFix) newTmp)
+       (cons elem (cdr newFix)))
+      (list
+       (cons elem newTmp)
+       newFix)))
+    (cons (car Tmp) (TRANSFER_MIN (cdr Tmp) Fix)))
+   (list
+    (cdr Tmp)
+    (cons (car Tmp) Fix)))
+  (list Tmp Fix)))
+
+
+(setq Net
+'((1 (2 . 4)(3 . 7))
+(2 (1 . 4)(3 . 3)(6 . 1))
+(3 (1 . 7)(2 . 3)(4 . 2)(5 . 5))
+(4 (3 . 2)(5 . 1)(6 . 7))
+(5 (3 . 5)(4 . 1)(6 . 3))
+(6 (2 . 1)(4 . 7)(5 . 3))))
+
+(print (F Net))
